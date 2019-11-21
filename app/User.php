1fd3da60a5ef9083e9d2c8d6e4c33f30e5fe8233
Rajan <rajan@freshbits.in>
2019-11-21 14:34:35 +0530

    create users migration,disable timestamp
    Signed-off-by: Rajan <rajan@freshbits.in>

--- app/User.php
+++ app/User.php
@@ -11,29 +11,10 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * Indicates if the model should be timestamped.
      *
-     * @var array
+     * @var bool
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    public $timestamps = false;
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
